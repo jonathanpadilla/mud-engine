e900f3b6007a02b21a0001e80848e5eecb303149
Jonathan Padilla <jonathanpadilla09@outlook.com>
2025-03-26 00:30:36 -0300

    feat: update version

--- release.config.cjs
+++ release.config.cjs
@@ -1,3 +1,12 @@
 module.exports = {
-  branches: ["main", { name: "next", prerelease: true }]
+  branches: ["main", { name: "next", prerelease: true }],
+  tagFormat: "${version}",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    ["@semantic-release/npm", { "npmPublish": true }],
+    "@semantic-release/github",
+    "@semantic-release/git"
+  ]
 }
